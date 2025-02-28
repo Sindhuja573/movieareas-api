5c38538fab076dbed1ffba6aa7b8fafb03ede9c1
Sindhuja573 <sindhuja2k16@gmail.com>
2025-02-28 15:34:05 +0530

    new route structure

--- main.js
+++ main.js
@@ -7,6 +7,31 @@ app.get("/", (req, res) => {
   res.json({ message: "hello vanakam!", name: "sindhuja", phone: 9898787889 });
 });
 
+//crud functionality of movies
+
+//c -creating the movie
+
+app.post("/movies" , () => {
+
+});
+
+//r-reading of movie
+
+app.get("/movies" , () => {
+
+});
+
+// u-update of movie
+app.put("/movies:id" , () => {
+
+});
+
+// d-delete the movie
+app.delete("/movies:id" , () => {
+
+});
+
+
 app.listen(PORT, () => {
   console.log(`server is running at http://localhost:${PORT}`);
 });
